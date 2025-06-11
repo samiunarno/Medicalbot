cfdb6996630e74cddcf0124e9fbf3f9bffcf9f27
samiunarno <samiunarno.iub@gmail.com>
2025-06-12 01:15:08 +0600

    new commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative Medical AI",
+    version = '0.0.0',
+    author = "Samiun Mahmud",
+    author_email = 'samiunarnouk@gmail.com',
+    packages = find_packages(),
+    install_require = []
+)
